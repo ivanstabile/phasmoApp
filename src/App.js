7157ddc08124dead46658aa92d0832f7cbe12965
ivan stabile <ivan_stabile@hotmail.com>
2022-07-23 14:57:11 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import tmi from "tmi.js";
+
+import "./styles.css";
+
+const client = new tmi.Client({
+  channels: ["ivansttv"]
+});
+
+export default function App() {
+  const [votes, setVotes] = useState([]);
+  const [status, setStatus] = useState("stopped");
+
+  useEffect(() => {
+    client.connect().catch(console.error);
+
+    function onMessage(channel, tags, message, self) {
+      console.log(self, message, status);
+
+      if (tags["username"] === "ivansttv") {
+        if (message === "!start") {
+          setStatus("playing");
+        } else if (message === "!stop") {
+          setStatus("stopped");
+        } else if (message === "!reset") {
+          setStatus("stopped");
+          setVotes([]);
+        }
+
+        return;
+      }
+
+      if (status !== "playing") return;
+
+      if (message.startsWith("!vote")) {
+        let [, ghost] = message.split("!vote ");
+        ghost = ghost.trim().toLowerCase();
+
+        setVotes((votes) =>
+          votes
+            .filter((vote) => vote.user !== tags["display-name"])
+            .concat({ user: tags["display-name"], vote: ghost })
+        );
+      }
+    }
+
+    client.on("message", onMessage);
+
+    return () => {
+      client.off("message", onMessage);
+    };
+  }, [status]);
+
+  return (
+    <div className="App">
+      {["spirit", "wraith", "phantom", "poltergeist", "banshee", "jinn"].map(
+        (ghost) => {
+          const matches = votes.filter((vote) => vote.vote === ghost);
+
+          return (
+            <div key={ghost}>
+              <div className="ghost">
+                {ghost} {matches.length > 0 && `(x${matches.length})`}
+              </div>
+              <div className="votes">
+                {matches.map((match) => match.user).join(", ")}
+              </div>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
+}
